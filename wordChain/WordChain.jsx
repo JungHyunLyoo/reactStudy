777cfe20c1076d6f1ddc36ffd618467ab0fb4667
JungHyunLyoo <dkcmsadkcmsa@gmail.com>
2020-02-29 23:47:38 +0900

    [wordChain]-modified class style to function style

--- wordChain/WordChain.jsx
+++ wordChain/WordChain.jsx
@@ -1,55 +1,42 @@
 const React = require('react');
-const { Component } = React;
+const { useState, useRef } = React;
 
-class WordChain extends Component {
-    state = {
-        word: '유정현',
-        value: '',
-        result: ''
-    };
+const WordChain = () => {
 
-    onSubmitForm = (e) => {
+    const [word, setWord] = useState('유정현');
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const inputRef = useRef(null);
+
+    const onSubmitForm = (e) => {
         e.preventDefault();
-        if (this.state.word[this.state.word.length-1] === this.state.value[0]) {
-            this.setState({
-                word: this.state.value,
-                value: '',
-                result: '딩동댕'
-            });
-            this.input.focus();
+        if (word[word.length-1] === value[0]) {
+            setWord(value);
+            setValue('');
+            setResult('딩동댕');
+            inputRef.current.focus();
         } else {
-            this.setState({
-                value: '',
-                result: '땡'
-            });
-            this.input.focus();
+            setValue('');
+            setResult('꽝');
+            inputRef.current.focus();
         }
     };
 
-    onChangeInput = (e) => {
-        this.setState({
-            value: e.target.value
-        });
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
     };
 
-    input;
-
-    onRefInput = (c) => {
-        this.input = c;
-    };
-
-    render() {
-        return (
-            <>
-                <div>{this.state.word}</div>
-                <form onSubmit={this.onSubmitForm}>
-                    <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput}></input>
-                    <button>입력!</button>
-                </form>
-                <div>{this.state.result}</div>
-            </>
-        );
-    }
+    return (
+        <>
+            <div>{word}</div>
+            <form onSubmit={onSubmitForm}>
+                <label htmlFor={"wordInput"}>글자를 입력하세요.</label>
+                <input className="class" id={"wordInput"} ref={inputRef} value={value} onChange={onChangeInput}></input>
+                <button>입력!</button>
+            </form>
+            <div>{result}</div>
+        </>
+    );
 }
 
 module.exports = WordChain;
