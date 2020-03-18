5fc4f231cfd6bdd4cdc8b047065810ae3fb57d65
JungHyunLyoo <dkcmsadkcmsa@gmail.com>
2020-03-18 23:42:46 +0900

    [responseCheck]-render method modified

--- responseCheck/ResponseCheck.js
+++ responseCheck/ResponseCheck.js
@@ -1,14 +1,38 @@
-const React = require('react');
-const {Component} = React;
+import React, {Component} from 'react';
+
 
 class ResponseCheck extends Component {
     state = {
-        text: 'hello, webpack',
-    }
+        state: 'waiting',
+        message: '클릭해서 시작하세요.',
+        result: [],
+    };
+
+    onClickScreen = () => {
+
+    };
+
+    renderAverage = () => {
+        const {result} = this.state;
+        return result.length === 0 ?
+            null : <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>;
+    };
 
     render() {
-        return <h1>{this.state.text}</h1>
+        const {state, message} = this.state;
+        return (
+            <>
+                <div
+                    id="screen"
+                    className={this.state.state}
+                    onClick={this.onClickScreen}
+                >
+                    {this.state.message}
+                </div>
+                {this.renderAverage()}
+            </>
+        )
     }
 }
 
-module.exports = ResponseCheck;
+export default ResponseCheck;
