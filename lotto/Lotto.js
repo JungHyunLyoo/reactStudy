0268fabbe9d376a564c8f41b1a08387494b8377d
JungHyunLyoo <dkcmsadkcmsa@gmail.com>
2020-04-11 19:16:18 +0900

    [lotto]-project init

---
+++ lotto/Lotto.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Lotto extends Component {
+    render() {
+        return (
+            <>
+                testtt
+            </>
+        )
+    }
+}
+
+export default Lotto;
