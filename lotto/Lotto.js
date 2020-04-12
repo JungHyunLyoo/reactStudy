ae3641514df01223b31a6c8cd51cd36a1a0e300c
JungHyunLyoo <dkcmsadkcmsa@gmail.com>
2020-04-12 19:12:48 +0900

    [lotto]-function style

--- lotto/Lotto.js
+++ lotto/Lotto.js
@@ -1,8 +1,7 @@
-import React, {Component} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import Ball from './Ball';
 
 function getWinNumbers() {
-    console.log('getWinNumbers');
     const candidate = Array(45).fill().map((v, i) => i + 1);
     const shuffle = [];
     while (candidate.length > 0) {
@@ -13,77 +12,51 @@ function getWinNumbers() {
     return [...winNumbers, bonusNumber];
 }
 
-class Lotto extends Component {
-    state = {
-        winNumbers: getWinNumbers(),
-        winBalls: [],
-        bonus: null,
-        redo: false,
-    };
+const Lotto = () => {
+    const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+    const [winBalls, setWinBalls] = useState([]);
+    const [bonus, setBonus] = useState(null);
+    const [redo, setRedo] = useState(false);
+    const timeouts = useRef([]);
 
-    timeouts = [];
-
-    runTimeouts = () => {
-        const {winNumbers} = this.state;
+    useEffect(() => {
         //es6 이후로, setTimeout에 들어가는 변수의 타입이 let인 경우 클로저문제가 생기지 않는다고 한다.
-        for (let i = 0; i < this.state.winNumbers.length - 1; i++) {
-            this.timeouts[i] = setTimeout(() => {
-                this.setState((prevState) => {
-                    console.log(1);
-                    return {
-                        winBalls: [...prevState.winBalls, winNumbers[i]]
-                    };
-                });
+        for (let i = 0; i < winNumbers.length - 1; i++) {
+            timeouts.current[i] = setTimeout(() => {
+                setWinBalls((prev) => [...prev, winNumbers[i]]);
             }, (i + 1) * 1000);
         }
-        this.timeouts[6] = setTimeout(() => {
-            this.setState({
-                bonus: winNumbers[6],
-                redo: true,
-            });
+        timeouts.current[6] = setTimeout(() => {
+            setBonus(winNumbers[6]);
+            setRedo(true);
         }, 7000);
-    }
+        return () => {
+          timeouts.current.forEach((v)=>{
+              clearTimeout(v);
+          });
+        };
+    }, [timeouts.current]);
+    //조건에 표현식도 됨!!!! 원래 표현식 자리였던건가?
 
-    componentDidMount() {
-        this.runTimeouts();
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (this.state.winBalls.length === 0) {
-            this.runTimeouts();
-        }
-    }
-
-    onClickRedo = () => {
-        this.setState({
-            winNumbers: getWinNumbers(),
-            winBalls: [],
-            bonus: null,
-            redo: false,
-        });
-        this.timeouts = [];
+    const onClickRedo = () => {
+        setWinNumbers(getWinNumbers());
+        setWinBalls([]);
+        setBonus(null);
+        setRedo(false);
+        timeouts.current = [];
     };
 
-    componentWillUnmount() {
-        this.timeouts.forEach((t) => {
-            clearTimeout(t);
-        })
-    }
-
-    render() {
-        const {winBalls, bonus, redo} = this.state;
-        return (
-            <>
-                <div>당첨 숫자</div>
-                <div id="결과창">
-                    {winBalls.map((v) => <Ball key={v} number={v}/>)}
-                </div>
-                <div>보너스!</div>
-                {bonus && <Ball number={bonus}/>}
-                {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
-            </>
-        );
-    }
+    return (
+        <>
+            <div>당첨 숫자</div>
+            <div id="결과창">
+                {winBalls.map((v) => <Ball key={v} number={v}/>)}
+            </div>
+            <div>보너스!</div>
+            {bonus && <Ball number={bonus}/>}
+            {redo && <button onClick={onClickRedo}>한 번 더!</button>}
+        </>
+    );
 }
 
 export default Lotto;
