24871495a63dad2c4d840415036f30cfcdc8417b
JungHyunLyoo <dkcmsadkcmsa@gmail.com>
2020-04-19 21:20:13 +0900

    [tictactoe]-project init

---
+++ ticTacToe/TicTacToe.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TicTacToe extends Component {
+    render(){
+        return (
+            <>
+            Test1232131
+            </>
+        )
+    }
+}
+
+export default TicTacToe;
