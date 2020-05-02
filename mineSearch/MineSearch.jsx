b3a4ede5a926dd1efe9db8f9d2dd3bc7136577ca
JungHyunLyoo <dkcmsadkcmsa@gmail.com>
2020-05-03 01:26:06 +0900

    [mineSearch]-project init'

---
+++ mineSearch/MineSearch.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MineSearch extends Component {
+    render() {
+        return (
+            <>
+                <p>test1</p>
+            </>
+        )
+    }
+}
+
+export default MineSearch;
